bd00ca604923be55f6c3cbf2084dae6b09e89754
Masaru Tsuchiyama <m.tmatma@gmail.com>
2021-08-14 11:49:14 +0900

    add shell

--- squid/run.sh
+++ squid/run.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
+ACTION=$1
+
+OPTION=
+COMMAND=
+if [ x"$ACTION" = x"bash" ]; then
+    OPTION=-it
+    COMMAND=bash
+fi
+
 docker run \
-    -p 3128:3128 \
+    $OPTION -p 3128:3128 \
     -v $SCRIPT_DIR/conf/squid.conf:/etc/squid/squid.conf \
-    squid-cache:1 \
-    "$@"
+    squid-cache:1 $COMMAND
