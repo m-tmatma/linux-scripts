5147493dbac2cfd320d016d597cfd06eca964b80
Masaru Tsuchiyama <m.tmatma@gmail.com>
2021-10-17 08:42:58 +0900

    fix indent

--- ssh/setup-ssh-key.sh
+++ ssh/setup-ssh-key.sh
@@ -3,13 +3,13 @@
 SSH_KEY=~/id_rsa.pub
 
 if [ -e "$SSH_KEY" ]; then
-	mkdir -p ~/.ssh
-	cat $SSH_KEY >>  ~/.ssh/authorized_keys
-	chmod 755 ~
-	chmod 700 ~/.ssh
-	chmod 600 ~/.ssh/authorized_keys
-	rm  $SSH_KEY
+    mkdir -p ~/.ssh
+    cat $SSH_KEY >>  ~/.ssh/authorized_keys
+    chmod 755 ~
+    chmod 700 ~/.ssh
+    chmod 600 ~/.ssh/authorized_keys
+    rm  $SSH_KEY
 else
-	echo "$SSH_KEY doesn't exist"
+    echo "$SSH_KEY doesn't exist"
     exit 1
 fi
