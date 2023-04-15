772f8332377ce39663cdfa14264576be19069ff4
Masaru Tsuchiyama <m.tmatma@gmail.com>
2023-04-16 08:33:07 +0900

    remove and add .git_alias

--- git/install.sh
+++ git/install.sh
@@ -4,6 +4,10 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 bash_aliases=${HOME}/.bash_aliases
 git_alias=${HOME}/.git_alias
 
-cat $SCRIPT_DIR/git-alias   >> $git_alias
+cp -f $SCRIPT_DIR/git-alias $git_alias
+
+cat ~/.bash_aliases | grep -v .git_alias > ~/.bash_aliases2
+mv  ~/.bash_aliases2 ~/.bash_aliases
 echo ". ~/.git_alias" >> $bash_aliases
 
+tail $bash_aliases
